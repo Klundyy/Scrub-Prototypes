f97e0e336b3fb931247a94beefac16c72ad0cdaf
Ian-Stentz <ianstentz25@gmail.com>
2024-11-14 14:21:03 -0800

    Toy Complete
    * Controls Refined
    * Fine tuned a little
    * No pause, start or end state
    * Missing a tell for the window being completely cleaned

--- Skyrise Scrubbing/Assets/Scripts/PlayerMovement.cs
+++ Skyrise Scrubbing/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
     public GameObject platform;
     public GameObject leftRope;
     public GameObject rightRope;
-    private Vector2 velocity = Vector2.zero;
     public float baseSpeed = 4f;
     private float playerAngle = 0f;
     private float leftPull = 0f;
@@ -17,35 +15,46 @@ public class PlayerMovement : MonoBehaviour
     public float pullSpeed = 0.4f;
     public float fallCoefficient = 1f;
     public float maxAngleDeg = 30f;
+    public float pullDecay = 1.5f;
     private float ropeStartingHeight;
     private float ropeHorizontalOffset;
-    private float playerRadius = 1f;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         ropeStartingHeight = (leftRope.transform.localPosition.y + rightRope.transform.localPosition.y) / 2;
         ropeHorizontalOffset = leftRope.transform.localPosition.x;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            leftPull += pullSpeed * Time.deltaTime;
+        }
+        else
+        {
+            leftPull -= pullSpeed * Time.deltaTime * pullDecay;
+        }
+        leftPull = Mathf.Clamp(leftPull, 0f, 1f);
+        if (Input.GetKey(KeyCode.D))
+        {
+            rightPull += pullSpeed * Time.deltaTime;
+        }
+        else
+        {
+            rightPull -= pullSpeed * Time.deltaTime * pullDecay;
+        }
+        rightPull = Mathf.Clamp(rightPull, 0f, 1f);
+        playerAngle = maxAngleDeg * (rightPull - leftPull);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        velocity += new Vector2(0, -baseSpeed) * Time.deltaTime;
-        //eventually change controls
-        if(Input.GetKey(KeyCode.A)) {
-            leftPull += pullSpeed * Time.deltaTime;
-        } else {
-            leftPull -= pullSpeed * Time.deltaTime * fallCoefficient;
-        }
-        leftPull = Mathf.Clamp(leftPull, 0f, 1f);
-        if(Input.GetKey(KeyCode.D)) {
-            rightPull += pullSpeed * Time.deltaTime;
-        } else {
-            rightPull -= pullSpeed * Time.deltaTime * fallCoefficient;
-        }
-        rightPull = Mathf.Clamp(rightPull, 0f, 1f);
-        playerAngle = maxAngleDeg * (rightPull-leftPull);
-        velocity += new Vector2(0, baseSpeed * (leftPull + rightPull)) * Time.deltaTime;
+        rb.velocity += new Vector2(0, -baseSpeed * fallCoefficient) * Time.deltaTime;
+        rb.velocity += new Vector2(0, baseSpeed * (leftPull + rightPull)) * Time.deltaTime;
         float sin = Mathf.Sin(Mathf.Deg2Rad * playerAngle);
         float ropeHeighOffset = ropeHorizontalOffset * sin;
         rightRope.transform.localPosition = new Vector3(-ropeHorizontalOffset, ropeStartingHeight - ropeHeighOffset, 0);
@@ -53,9 +62,9 @@ public class PlayerMovement : MonoBehaviour
         platform.transform.localRotation = Quaternion.Euler(0, 0, playerAngle);
 
         //contentious line
-        velocity += new Vector2(sin * baseSpeed, 0) * Time.deltaTime;
+        rb.velocity += new Vector2(-sin * baseSpeed, 0) * Time.deltaTime;
 
-        this.transform.Translate(velocity * Time.deltaTime);
+        //this.transform.Translate(rb.velocity * Time.deltaTime);
         
         //collide with edges of screen
         
